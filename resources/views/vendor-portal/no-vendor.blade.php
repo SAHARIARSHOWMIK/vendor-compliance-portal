40150783164a721ed26b7ce29e50d52e501b9f89
SHAKIB SAHARIAR SHOWMIK <94044208+SAHARIARSHOWMIK@users.noreply.github.com>
2026-07-10 18:49:19 +0800

    Fix vendor portal Blade title syntax

--- resources/views/vendor-portal/no-vendor.blade.php
+++ resources/views/vendor-portal/no-vendor.blade.php
@@ -1,4 +1,4 @@
-<x-layout :title="No Vendor Linked">
+<x-layout title="No Vendor Linked">
     <div class="max-w-lg mx-auto mt-12 text-center">
         <div class="rounded bg-amber-50 border border-amber-200 text-amber-800 px-6 py-8">
             <h1 class="text-xl font-semibold mb-2">No Vendor Account Linked</h1>
